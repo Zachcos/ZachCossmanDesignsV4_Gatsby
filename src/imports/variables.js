ca26c2f731bfec15477c8e4475199a7efa2fc310
Zach Cossman <Zachcos@gmail.com>
2020-12-09 01:27:05 -0500

    Add missing comma

--- src/imports/variables.js
+++ src/imports/variables.js
@@ -1,8 +1,8 @@
 export const colors = {
   darkGrey: '#484D5C',
   lightGrey: '#C0CCD4',
-  accent: '#FFCC00'
-}
+  accent: '#FFCC00',
+};
 
 export const font = {
   h1: '4.209rem',
@@ -11,4 +11,4 @@ export const font = {
   h4: '1.777rem',
   h5: '1.333rem',
   base: '1rem',
-}
+};
