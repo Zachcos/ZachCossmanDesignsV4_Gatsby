fee1375eb2fde1d3effd04c99f43d0dff484b42a
Zach Cossman <Zachcos@gmail.com>
2020-12-09 01:33:36 -0500

    Add plugins to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,13 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    'gatsby-transformer-json',
+    {
+      resolve: 'gatsby-plugin-layout',
+      options: {
+        component: require.resolve('./src/components/layout.js'),
+      },
+    },
+  ],
+};
