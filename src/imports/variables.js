3f661b2e04a76d426e0aba3bb346a2e91599083f
Zach Cossman <Zachcos@gmail.com>
2020-12-09 01:17:57 -0500

    Create variables file

---
+++ src/imports/variables.js
@@ -0,0 +1,14 @@
+export const colors = {
+  darkGrey: '#484D5C',
+  lightGrey: '#C0CCD4',
+  accent: '#FFCC00'
+}
+
+export const font = {
+  h1: '4.209rem',
+  h2: '3.157rem',
+  h3: '2.369rem',
+  h4: '1.777rem',
+  h5: '1.333rem',
+  base: '1rem',
+}
