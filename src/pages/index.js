a2d8c99ec860e0c180451602d2acd9e8e9daeaae
Zach Cossman <Zachcos@gmail.com>
2020-12-10 23:11:54 -0500

    Add styles for hero display text and copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+import { colors, font } from '../imports/variables';
+
+const HeroContainer = styled.div`
+  margin-top: 100px;
+  width: 695px;
+`;
+
+const HeroDisplayText = styled.h1`
+  color: ${colors.accent};
+  font-family: Oswald, sans-serif;
+  font-size: ${font.h1};
+  line-height: 1.2em;
+  margin-bottom: 50px;
+  text-transform: uppercase;
+`;
+
+const HeroDisplayCopy = styled.p`
+  line-height: 2em;
+`;
 
 export default function Home() {
-  return <div>Hello world!</div>;
+  return (
+    <HeroContainer>
+      <HeroDisplayText>
+        Hi, I'm Zach — I design and develop websites
+      </HeroDisplayText>
+      <HeroDisplayCopy>
+        For over 10 years I've been designing and developing sites and web
+        experiences for companies and creative individuals. Check out some of my
+        work, learn more about me, and get in touch...
+      </HeroDisplayCopy>
+    </HeroContainer>
+  );
 }
