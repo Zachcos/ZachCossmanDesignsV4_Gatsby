760554ab282f43cdda2df47bd85a201028505309
Zach Cossman <Zachcos@gmail.com>
2020-12-11 00:38:58 -0500

    Add hover interactions for Navbar items

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -23,12 +23,67 @@ const LogoContainer = styled.div`
 `;
 
 const NavbarItems = styled.div`
+  align-items: center;
   display: flex;
   font-family: Oswald, sans-serif;
   font-size: ${font.base};
   justify-content: space-between;
   margin-right: 60px;
   width: 400px;
+  a {
+    color: ${colors.lightGrey};
+    cursor: pointer;
+    padding: 5px;
+    position: relative;
+    text-decoration: none;
+    &.text-link:after {
+      background: ${colors.accent};
+      bottom: -10px;
+      content: '';
+      height: 3px;
+      left: 0;
+      opacity: 0;
+      position: absolute;
+      transition: 0.3s ease-in-out;
+      width: 100%;
+    }
+    &.text-link:hover:after {
+      bottom: -5px;
+      opacity: 1;
+    }
+    &.icon-link {
+      transition: all 0.3s ease-out;
+      z-index: 1;
+    }
+    &.icon-link:after {
+      content: '';
+      position: absolute;
+      left: 50%;
+      bottom: 50%;
+      width: 0%;
+      height: 0%;
+      transform: translate(-50%, 50%);
+      opacity: 1;
+      background: ${colors.accent};
+      border-radius: 50%;
+      transition: all 0.3s ease-out;
+      z-index: -1;
+    }
+    &.icon-link:hover {
+      color: ${colors.darkGrey};
+      &:after {
+        height: 100%;
+        width: 100%;
+      }
+    }
+  }
+  a > i {
+    align-items: center;
+    display: flex;
+    height: 40px;
+    justify-content: center;
+    width: 40px;
+  }
 `;
 
 export default function Navbar() {
@@ -39,15 +94,21 @@ export default function Navbar() {
           <img src={logo} alt="" />
         </LogoContainer>
         <NavbarItems>
-          <div>work</div>
-          <div>about</div>
-          <div>email me</div>
-          <div>
+          <a href="#" alt="" className="text-link">
+            work
+          </a>
+          <a href="#" alt="" className="text-link">
+            about
+          </a>
+          <a href="#" alt="" className="text-link">
+            email me
+          </a>
+          <a className="icon-link">
             <i className="fab fa-lg fa-behance" />
-          </div>
-          <div>
+          </a>
+          <a className="icon-link">
             <i className="fab fa-lg fa-github" />
-          </div>
+          </a>
         </NavbarItems>
       </NavbarContainer>
     </>
