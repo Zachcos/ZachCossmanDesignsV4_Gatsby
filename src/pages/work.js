7195cda3765a7b919609f16d318a289bd21a771c
Zach Cossman <Zachcos@gmail.com>
2020-12-16 13:22:34 -0500

    Add Work page file

---
+++ src/pages/work.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import styled from 'styled-components';
+import { colors, font } from '../imports/variables';
+
+export default function Work() {
+  return <h1>This is the work page</h1>;
+}
