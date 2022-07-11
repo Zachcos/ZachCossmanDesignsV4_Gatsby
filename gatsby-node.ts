4e6af62c86b875ff026d82e2dc66e3b99fee643e
Zach Cossman <Zachcos@gmail.com>
2022-07-11 13:31:46 -0400

    Add <void> type to promise

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -2,7 +2,7 @@ const path = require('path');
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
-  return new Promise((resolve, reject) => {
+  return new Promise<void>((resolve) => {
     graphql(`
       {
         allFeaturedPortfolioJson {
