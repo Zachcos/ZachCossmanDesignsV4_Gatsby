798e0f24f077dd048103d2c5316ffa2414b3f68d
Zach Cossman <Zachcos@gmail.com>
2022-07-13 10:32:25 -0400

    Update interfaces

--- index.d.ts
+++ index.d.ts
@@ -2,46 +2,42 @@ declare module '*.svg';
 declare module '*.jpg';
 
 interface ParentAniProps {
-  variants: {
-    initial: {
-      top: string;
+  initial: {
+    top: string;
+  };
+  animate: {
+    top: number;
+    transition: {
+      duration: number;
+      delayChildren: number;
+      staggerChildren: number;
+      ease: string;
     };
-    animate: {
-      top: number;
-      transition: {
-        duration: number;
-        delayChildren: number;
-        staggerChildren: number;
-        ease: string;
-      };
-    };
-    exit: {
-      top: string;
-      opacity: number;
-      transition: {
-        duration: number;
-        ease: string;
-      };
+  };
+  exit: {
+    top: string;
+    opacity: number;
+    transition: {
+      duration: number;
+      ease: string;
     };
   };
 }
 
 interface ChildAniProps {
-  variants: {
-    initial: {
-      opacity: number;
-      top: string;
-    };
-    animate: {
-      opacity: number;
-      top: number;
-      transition: {
-        duration: number;
-        ease: string;
-      };
-    };
-    exit: {};
+  initial?: {
+    opacity: number;
+    top: string;
   };
+  animate?: {
+    opacity: number;
+    top: number;
+    transition: {
+      duration: number;
+      ease: string;
+    };
+  };
+  exit?: {};
 }
 
 interface FeaturedItemProps {
@@ -59,4 +55,63 @@ interface LayoutProps {
     pathname: string;
   };
   children: JSX.Element;
+}
+
+interface DataProps {
+  data: {
+    featured: {
+      edges: {
+        node: {
+          id: string;
+          title: string;
+          slug: string;
+          portImg: string;
+        };
+        map: Function;
+      };
+    };
+    other: {
+      edges: {
+        node: {
+          id: string;
+          thumbImg: string;
+          behanceUrl: string;
+        };
+        map: Function;
+      };
+    };
+  };
+}
+
+interface FeaturedWorkItem {
+  node: {
+    id: string;
+    title: string;
+    slug: string;
+    portImg: string;
+  };
+}
+
+interface OtherWorkItem {
+  node: {
+    id: string;
+    thumbImg: string;
+    behanceUrl: string;
+  };
+}
+
+interface PortfolioItem {
+  data: {
+    featuredPortfolioJson: {
+      slug: string;
+      title: string;
+      description: string[];
+      liveUrl: string;
+      nextProj: {
+        title: string;
+        slug: string;
+      };
+      assets: string[];
+    };
+  };
 }
